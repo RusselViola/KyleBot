a12f8ec70a3838f9539a6a22680fdb78ad930553
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-01-24 18:57:16 -0500

    this could be bad for the safety of kylebot, just trying to get a connection from heroku to facebook

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
+    res.send('1717196505')
 })
 
 // for Facebook verification
