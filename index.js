0f11bd6384cf70bd9e1594b3e50532167ffbc70a
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-01-24 18:53:14 -0500

    fixed 'expresss' typo in index.js:6:13 to express

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-const app = expresss()
+const app = express()
 
 app.set('port', (process.env.PORT  || 5000))
 // Process application/x-www-form-urlencoded
