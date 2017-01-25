93b992cdc7656d00e3a8cc716ad60bd95adf8569
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-01-24 19:04:23 -0500

    adding semicolons, in a desperate attempt to not have to do this over

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 'use strict';
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
 
-app.set('port', (process.env.PORT  || 5000))
+app.set('port', (process.env.PORT  || 5000));
 // Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Process application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // Index route
 app.get('/', function (req, res) {
-    res.send('1717196505')
-})
+    res.send('Hello world, I am a chat bot');
+});
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] === 'were_gonna_get_kyle_so_laid') {
-        res.send(req.query['hub.challenge'])
+        res.send(req.query['hub.challenge']);
     }
-    res.send('Error, wrong token')
-})
+    res.send('Error, wrong token');
+});
 
 // Spin up the server
 app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
-})
+    console.log('running on port', app.get('port'));
+});
